1688d3336010041c7157fff7bf2193a3296c002d
xGabriel-Teles <gabriel.teles.santos@hotmail.com>
2024-05-02 21:34:39 -0300

    add exercicio03.js and delete teste.js

---
+++ exercicio03.js
@@ -0,0 +1,6 @@
+/*3. 
+Faça um algoritmo que pergunte a distância que um passageiro deseja percorrer em Km.
+Calcule o preço da passagem, cobrando R$ 0.50 por Km para viagens até 200 Km e R$ 0.45 para viagens mais longas.
+*/
+
+const prompt = require('prompt-sync')();
