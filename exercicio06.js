481539bfc5130e46a6a53174e42e9d3169428930
xGabriel-Teles <gabriel.teles.santos@hotmail.com>
2024-05-11 17:15:33 -0300

    exercicio06.js solved

--- exercicio06.js
+++ exercicio06.js
@@ -4,3 +4,18 @@ O jogador vai tentar descobrir qual foi o valor sorteado.
 */
 
 const prompt = require('prompt-sync')();
+
+let correctNumber = Math.floor(Math.random()*100/20)+1;
+
+let guessNumber;
+let attempts = 0;
+while (guessNumber !== correctNumber){
+    guessNumber = parseInt(prompt('Digite um número entre 1 e 5: '));
+    attempts++
+        if(guessNumber === correctNumber){
+            console.log(`Você acertou, o número correto era ${correctNumber}, você precisou de ${attempts} tentativas para acertar.`);
+        } else {
+            console.log('Errou, tente novamente!');
+        }
+    
+}
