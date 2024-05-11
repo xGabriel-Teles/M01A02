5be24872118c153adfa19829eba76c9db187e0be
xGabriel-Teles <gabriel.teles.santos@hotmail.com>
2024-05-11 17:54:20 -0300

    exercicio08.js and exercicio09.js files added

---
+++ exercicio09.js
@@ -0,0 +1,5 @@
+/* 9. 
+Desenvolva um aplicativo que leia o salário e o sexo de vários funcionários. 
+No final, mostre o total de salário pago aos homens e o total pago às mulheres. 
+O programa vai perguntar ao usuário se ele quer continuar ou não sempre que ler os dados de um funcionário.
+*/
