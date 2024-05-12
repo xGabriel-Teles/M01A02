98803a13637d4aeda73d404f5831409b85a0da7b
xGabriel-Teles <gabriel.teles.santos@hotmail.com>
2024-05-12 01:39:08 -0300

    exercicio08.js solved

--- exercicio08.js
+++ exercicio08.js
@@ -8,3 +8,32 @@ Cada hora de atividade física no mês vale pontos. O sistema funciona assim:
 Faça um programa que leia quantas horas de atividade uma pessoa teve por mês.
 Calcule e mostre quantos pontos ela teve e quanto dinheiro ela conseguiu ganhar.
 */
+
+const prompt = require('prompt-sync')();
+
+console.log('========================================================================');
+let activityHours = parseInt(prompt('Quantas horas de atividade você teve esse mês? '));
+let points;
+
+function convertPointsToMoney(points) {
+    let pointValue = 0.05;
+    return (points * pointValue).toFixed(2);
+}
+
+if (isNaN(activityHours - 1)) {
+    console.log(`Você não digitou um número.`)
+} else if (activityHours < 0) {
+    console.log(`Você digitou um número negativo (${activityHours}).`)
+} else {
+    if (activityHours >= 0 && activityHours <= 10) {
+        points = activityHours * 2;
+    } else if (activityHours > 10 && activityHours <= 20) {
+        points = activityHours * 5;
+    } else {
+        points = activityHours * 10;
+    }
+    console.log(`\nVocê conseguiu ${points} pontos, sendo assim, ganhou R$${convertPointsToMoney(points)}.`)
+    console.log('========================================================================');
+}
+
+
