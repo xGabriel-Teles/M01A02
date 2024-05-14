245616b8f6a498d55281b0f08bb48e797d4602ab
xGabriel-Teles <gabriel.teles.santos@hotmail.com>
2024-05-14 13:06:45 -0300

    add exercicio12.js and exercicio13.js

---
+++ exercicio12.js
@@ -0,0 +1,4 @@
+/*12. 
+Faça um programa que mostre os 10 primeiros elementos da Sequência de Fibonacci.
+Ex.: 1, 1, 2, 3, 5, 8, 13, 21. 
+*/
---
+++ exercicio13.js
@@ -0,0 +1,4 @@
+/*13. 
+Crie um programa que preencha automaticamente (usando lógica, não apenas atribuindo diretamente) 
+um vetor numérico com 15 posições com os primeiros elementos da sequência de Fibonacci.
+ */
