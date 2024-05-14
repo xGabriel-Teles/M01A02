d612dd6f9990601eaae42e50d45887bc777ccca4
xGabriel-Teles <gabriel.teles.santos@hotmail.com>
2024-05-14 19:35:03 -0300

    exercicio12.js and exercicio13.js solved

--- exercicio12.js
+++ exercicio12.js
@@ -2,3 +2,12 @@
 Faça um programa que mostre os 10 primeiros elementos da Sequência de Fibonacci.
 Ex.: 1, 1, 2, 3, 5, 8, 13, 21. 
 */
+
+let fibonacciSequence = [1,1]
+
+for (let i = 0; i < 8 ; i++){
+    let f1 = fibonacciSequence[0 + i];
+    let f2 = fibonacciSequence[1 + i];
+    fibonacciSequence.push(f1 + f2);
+}
+console.log(`\nOs 10 primeiros elementos da Sequência de Fibonacci são: ${fibonacciSequence}.`);
--- exercicio13.js
+++ exercicio13.js
@@ -2,3 +2,12 @@
 Crie um programa que preencha automaticamente (usando lógica, não apenas atribuindo diretamente) 
 um vetor numérico com 15 posições com os primeiros elementos da sequência de Fibonacci.
  */
+
+let fibonacciSequence = [1,1]
+
+for (let i = 0; i < 13 ; i++){
+    let f1 = fibonacciSequence[0 + i];
+    let f2 = fibonacciSequence[1 + i];
+    fibonacciSequence.push(f1 + f2);
+}
+console.log(fibonacciSequence);
