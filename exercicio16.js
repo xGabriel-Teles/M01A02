3ec684118154146ef3db2b80f54d196a9852ab3d
xGabriel-Teles <gabriel.teles.santos@hotmail.com>
2024-05-16 20:52:20 -0300

    exercicio16.js solved

--- exercicio16.js
+++ exercicio16.js
@@ -2,3 +2,14 @@
 Crie uma lógica que preencha um vetor de 20 posições com números aleatórios (entre 0 e 99) gerados pelo computador. 
 Logo em seguida, mostre os números gerados e depois coloque o vetor em ordem crescente, mostrando no final os valores ordenados.
  */
+
+let numbers = [];
+
+for (let i = 0; i < 20; i++) {
+    numbers.push(parseInt(Math.random()*100));
+}
+
+console.log(`Números: ${numbers.join(', ')}.`);
+console.log(`Números ordenados: ${numbers.sort(function(a, b){
+    return a - b;
+}).join(', ')}.`);
