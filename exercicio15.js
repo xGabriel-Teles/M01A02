fc0b4458bf1b05d2fd3eb4664ca883cedc36ed8f
xGabriel-Teles <gabriel.teles.santos@hotmail.com>
2024-05-16 13:49:22 -0300

    exercicio15.js solved

--- exercicio15.js
+++ exercicio15.js
@@ -2,3 +2,25 @@
 Desenvolva um programa que leia 10 números inteiros e guarde-os em um vetor. 
 No final, mostre quais são os números pares que foram digitados e em que posições eles estão armazenados.
 */
+
+const prompt = require('prompt-sync')();
+
+const numbers = [];
+
+console.log('========================================================================');
+for (let i = 0; i < 10; i ++){
+    let number = parseInt(prompt('Digite o número para ser adicionado ao vetor: '));
+    if (isNaN(number - 1)) {
+        console.log('Você não digitou um número');
+        i--;
+    } else {
+        numbers.push(number);
+    }
+} 
+console.log('');
+for (let j = 0; j < numbers.length; j++) {
+    if (numbers[j] % 2 === 0) {
+        console.log(`O número ${numbers[j]} é um número par e está armazenado na posição ${j} do vetor.`)
+    }
+}
+console.log('========================================================================');
