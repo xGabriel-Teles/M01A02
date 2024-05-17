a0a26cc9d2bc4fd44160e43265e3e3d8bed82add
xGabriel-Teles <gabriel.teles.santos@hotmail.com>
2024-05-17 20:24:45 -0300

    exercicio19.js solved

--- exercicio19.js
+++ exercicio19.js
@@ -3,3 +3,15 @@ Escrever um programa para ler 5 horários.
 Validar cada horário fornecendo através de repetição. 
 Escrever cada um deles no formato HH.MM.SS.
 */
+
+function getTimetable() {
+    let x = new Date();
+    let hours = x.getHours();
+    let min = x.getMinutes();
+    let seconds = x.getSeconds();
+
+    console.log(`${hours}:${min}:${seconds}.`)
+}
+for (let i = 1; i < 6; i++) {
+    setTimeout(getTimetable, i*1000)
+}
