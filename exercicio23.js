b5c9456fa64c49b77e096072177fccbd19a27f01
xGabriel-Teles <gabriel.teles.santos@hotmail.com>
2024-06-03 23:08:28 -0300

    exercicio23.js solved

--- exercicio23.js
+++ exercicio23.js
@@ -1,3 +1,22 @@
 /* 23. 
 Criar e imprimir a matriz identidade MI[1..7,1..7] em que todos os elementos da diagonal principal são iguais a 1 e os demais são nulos.
 */
+
+let lines = 7; 
+let columns = 7; 
+let MI = new Array(columns); 
+for (let i = 0; i < columns; i++) {
+	MI[i] = new Array(lines); 
+}
+
+for (let i = 0; i < MI.length; i++){
+    for(let j = 0; j < MI.length; j++){
+        if(MI[i] === MI[j]){
+            MI[i][j]= 1;
+        } else {
+            MI[i][j]= null;
+        }
+    }
+}
+
+console.table(MI);
