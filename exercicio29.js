0cce0fce8895a1a9553b9985346ef5013fe808a2
xGabriel-Teles <gabriel.teles.santos@hotmail.com>
2024-06-05 19:05:14 -0300

    exercise started

--- exercicio29.js
+++ exercicio29.js
@@ -6,3 +6,19 @@ Escreva um algoritmo que leia uma matriz M(5,5) e calcule as somas:
     d) todos os elementos da matriz M.
 Escrever essas somas e a matriz.
 */
+
+// Criando matriz M 
+let lines = 5;
+let columns = 5;
+let M = new Array(lines);
+for (let i = 0; i < lines; i++) {
+    M[i] = new Array(columns);
+}
+// Gerando os números de forma aleatória para a matriz
+for (let i = 0; i < lines; i++) {
+    for (let j = 0; j < columns; j++) {
+        M[i][j] = parseInt(Math.random() * 10);
+    }
+}
+
+console.table(M);
