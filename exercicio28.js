cc87628229210c6d74c92c94c7dc4997997c0909
xGabriel-Teles <gabriel.teles.santos@hotmail.com>
2024-06-05 19:02:06 -0300

    exercicio28.js solved

--- exercicio28.js
+++ exercicio28.js
@@ -3,3 +3,35 @@ Fazer um algoritmo para receber uma matriz 10 x 10 e devolver o resultado pedido
     a) a soma dos elementos acima da diagonal principal;
     b) a soma dos elementos abaixo da diagonal principal;
 */
+
+let aboveMainDiagonal = 0;
+let belowMainDiagonal = 0;
+
+// Criando matriz M 
+let lines = 10;
+let columns = 10;
+let M = new Array(lines);
+for (let i = 0; i < lines; i++) {
+    M[i] = new Array(columns);
+}
+// Gerando os números de forma aleatória para a matriz
+for (let i = 0; i < lines; i++) {
+    for (let j = 0; j < columns; j++) {
+        M[i][j] = parseInt(Math.random() * 10);
+    }
+}
+// Somando valores acima da diagonal principal e abaixo da diagonal principal
+for (let i = 0; i < lines; i++) {
+    for (let j = 0; j < columns; j++) {
+        if (i < j) {
+            aboveMainDiagonal += M[i][j];
+        }
+        if (i > j) {
+            belowMainDiagonal += M[i][j];
+        }
+    }
+}
+console.table(M)
+console.log(`\nA soma dos valores acima da diagonal principal é: ${aboveMainDiagonal}.`)
+console.log(`A soma dos valores abaixo da diagonal principal é: ${belowMainDiagonal}.`)
+console.log()
