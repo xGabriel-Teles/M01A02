9fb7fe835978c4bf4c5a01c06530d861cd4a96de
xGabriel-Teles <gabriel.teles.santos@hotmail.com>
2024-06-06 19:07:40 -0300

    under development

--- exercicio30.js
+++ exercicio30.js
@@ -2,3 +2,19 @@
 Escrever um algoritmo que lê uma matriz M(5,5) e cria 2 vetores SL(5) e SC(5) que contenham, respectivamente, as somas das linhas e das colunas de M. 
 Escrever a matriz e os vetores criados.
 */
+
+// Criando matriz M 
+let lines = 5;
+let columns = 5;
+let M = new Array(lines);
+for (let i = 0; i < lines; i++) {
+    M[i] = new Array(columns);
+}
+// Gerando os números de forma aleatória para a matriz
+for (let i = 0; i < lines; i++) {
+    for (let j = 0; j < columns; j++) {
+        M[i][j] = parseInt(Math.random() * 10);
+    }
+}
+
+console.table(M)
