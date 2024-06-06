6702ee945eb8d36fbf8c09bd141886a154415984
xGabriel-Teles <gabriel.teles.santos@hotmail.com>
2024-06-06 18:49:55 -0300

    exercicio29.js solved

--- exercicio29.js
+++ exercicio29.js
@@ -7,6 +7,12 @@ Escreva um algoritmo que leia uma matriz M(5,5) e calcule as somas:
 Escrever essas somas e a matriz.
 */
 
+
+let sumLineFour = 0;
+let sumColumnTwo = 0;
+let diagonalSum = 0;
+let sumTotal = 0;
+
 // Criando matriz M 
 let lines = 5;
 let columns = 5;
@@ -20,5 +26,34 @@ for (let i = 0; i < lines; i++) {
         M[i][j] = parseInt(Math.random() * 10);
     }
 }
+// Percorrendo a matriz
+for (let i = 0; i < lines; i++) {
+    for (let j = 0; j < columns; j++) {
+
+        // Soma dos valores da diagonal principal;
+        if (i === j) {
+            diagonalSum += M[i][j];
+        }
+
+        // Soma da linha 4;
+        if (j === 3) {
+            sumLineFour += M[i][j];
+        }
+
+        // Soma da coluna 2;
+        if (i === 1) {
+            sumColumnTwo += M[i][j];
+        }
+
+        // Soma do total
+        sumTotal += M[i][j];
+    }
+}
+
 
 console.table(M);
+console.log(`\nA soma dos valores da diagonal principal é:  ${diagonalSum}.`);
+console.log(`A soma dos valores da coluna dois é:  ${sumColumnTwo}.`);
+console.log(`A soma dos valores da linha quatro é:  ${sumLineFour}.`);
+console.log(`A soma de todos os valores da matriz é:  ${sumTotal}.`);
+console.log()
