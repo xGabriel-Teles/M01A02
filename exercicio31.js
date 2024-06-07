a91c62c9b8b5fefe59d4f42e2475b0424ae8ebff
xGabriel-Teles <gabriel.teles.santos@hotmail.com>
2024-06-07 12:57:06 -0300

    exercicio31.js solved

--- exercicio31.js
+++ exercicio31.js
@@ -3,3 +3,49 @@ Escreva um algoritmo que leia um número inteiro A e uma matriz V 30 x 30 de int
 Conte quantos valores iguais a A estão na matriz. 
 Crie, a seguir, uma matriz X contendo todos os elementos de V diferentes de A. Mostre os resultados.
 */
+
+const prompt = require('prompt-sync')();
+
+
+const A = parseInt(prompt('Digite um número inteiro de 0 até 9: '));
+let counter = 0;
+
+// Criando matriz M 
+let lines = 30;
+let columns = 30;
+let V = new Array(lines);
+for (let i = 0; i < lines; i++) {
+    V[i] = new Array(columns);
+}
+// Gerando os números de forma aleatória para a matriz
+for (let i = 0; i < lines; i++) {
+    for (let j = 0; j < columns; j++) {
+        V[i][j] = parseInt(Math.random() * 10);
+    }
+}
+
+let X = new Array(lines);
+for (let i = 0; i < lines; i++) {
+    X[i] = new Array(columns);
+}
+
+// Percorrendo a matriz V
+for (let i = 0; i < lines; i++) {
+    for (let j = 0; j < columns; j++) {
+
+        if (V[i][j] === A) {
+            counter++
+            continue;
+        } else {
+            X[i][j] = V[i][j];
+        }
+    }
+}
+console.log()
+console.log('Matriz V');
+console.table(V);
+console.log()
+console.log('Matriz X')
+console.table(X);
+
+console.log(`\nPossuem ${counter} números iguais a A na matriz V.`)
--- exercicio32.js
+++ exercicio32.js
@@ -2,3 +2,19 @@
 Escrever um algoritmo que lê uma matriz M(12,13) e divida todos os 13 elementos de cada uma das 12 linhas de M pelo maior elemento em módulo daquela linha.
 Escrever a matriz lida e a modificada.
 */
+
+// Criando matriz M 
+let lines = 12;
+let columns = 13;
+let M = new Array(lines);
+for (let i = 0; i < lines; i++) {
+    M[i] = new Array(columns);
+}
+// Gerando os números de forma aleatória para a matriz
+for (let i = 0; i < lines; i++) {
+    for (let j = 0; j < columns; j++) {
+        M[i][j] = parseInt(Math.random() * 10);
+    }
+}
+
+console.table(M)
