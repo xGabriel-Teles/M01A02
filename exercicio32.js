415de441131d68cb9c017856ef75575b0fd18422
xGabriel-Teles <gabriel.teles.santos@hotmail.com>
2024-06-07 13:23:38 -0300

    exercicio32.js solved

--- exercicio32.js
+++ exercicio32.js
@@ -3,6 +3,8 @@ Escrever um algoritmo que lê uma matriz M(12,13) e divida todos os 13 elementos
 Escrever a matriz lida e a modificada.
 */
 
+let absoluteGreatestLine = 0;
+
 // Criando matriz M 
 let lines = 12;
 let columns = 13;
@@ -13,8 +15,20 @@ for (let i = 0; i < lines; i++) {
 // Gerando os números de forma aleatória para a matriz
 for (let i = 0; i < lines; i++) {
     for (let j = 0; j < columns; j++) {
-        M[i][j] = parseInt(Math.random() * 10);
+        M[i][j] = parseInt(Math.random() * 20 - 10);
     }
 }
+console.log('\nMatriz original:')
+console.table(M)
 
+// Percorrendo a matriz
+for (let i = 0; i < lines; i++) {
+
+    absoluteGreatestLine = Math.max(...M[i].map(Math.abs));
+
+    for (let j = 0; j < columns; j++) {
+        M[i][j] = (M[i][j] / absoluteGreatestLine).toFixed(2);
+    }
+}
+console.log('\nMatriz modificada:')
 console.table(M)
