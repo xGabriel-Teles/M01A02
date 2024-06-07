a0c04dcf113130fbddac106393b55aad02ce24be
xGabriel-Teles <gabriel.teles.santos@hotmail.com>
2024-06-06 22:40:05 -0300

    add exercicio31.js and 32 files

---
+++ exercicio31.js
@@ -0,0 +1,5 @@
+/* 31. 
+Escreva um algoritmo que leia um número inteiro A e uma matriz V 30 x 30 de inteiros.
+Conte quantos valores iguais a A estão na matriz. 
+Crie, a seguir, uma matriz X contendo todos os elementos de V diferentes de A. Mostre os resultados.
+*/
---
+++ exercicio32.js
@@ -0,0 +1,4 @@
+/* 32. 
+Escrever um algoritmo que lê uma matriz M(12,13) e divida todos os 13 elementos de cada uma das 12 linhas de M pelo maior elemento em módulo daquela linha.
+Escrever a matriz lida e a modificada.
+*/
