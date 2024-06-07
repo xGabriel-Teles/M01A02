8607bf85d4e8f4ac038b38552fd1aca0847eec90
xGabriel-Teles <gabriel.teles.santos@hotmail.com>
2024-06-07 13:27:35 -0300

    add exercicio33 and 34 files

---
+++ exercicio33.js
@@ -0,0 +1,3 @@
+/* 33. 
+Faça um algoritmo que leia uma matriz 3 x 3 e após a leitura, multiplique os elementos da diagonal principal com a média dos elementos da diagonal secundária.
+*/
---
+++ exercicio34.js
@@ -0,0 +1,4 @@
+/* 34.
+Faça um algoritmo que leia uma matriz 50 x 50 de números reais. 
+A seguir, multiplique cada linha pelo elemento da diagonal principal daquela linha. Mostre a matriz após as multiplicações.
+*/
