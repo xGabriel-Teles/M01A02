b17ff7a196253e3db62f47bea018958d5dc2830f
xGabriel-Teles <gabriel.teles.santos@hotmail.com>
2024-06-10 13:40:49 -0300

    exercicio35.js solved

--- exercicio35.js
+++ exercicio35.js
@@ -3,3 +3,32 @@ Elaborar um algoritmo que leia um conjunto de 30 valores e os coloca em 2 vetore
 O tamanho do vetor é de 5 posições. Se algum vetor estiver cheio, escrevê-lo. 
 Terminada a leitura, escrever o conteúdo dos dois vetores. Cada vetor pode ser preenchido quantas vezes forem necessárias.
 */
+
+
+// Criando vetores
+
+let evenNumbers = [];
+let oddNumbers = [];
+
+for (let i = 0; i < 30; i++) {
+    let randomNumber = parseInt(Math.random() * 10 + 1);
+    if (randomNumber % 2 === 0) {
+        if (evenNumbers.length < 5){
+            evenNumbers.push(randomNumber);
+        } else {
+            console.log(`Vetor de números pares: ${evenNumbers}`);
+            evenNumbers = []
+            evenNumbers.push(randomNumber);
+        }
+    } else {
+        if (oddNumbers.length < 5){
+            oddNumbers.push(randomNumber);
+        } else {
+            console.log(`Vetor de números ímpares: ${oddNumbers}`);
+            oddNumbers = []
+            oddNumbers.push(randomNumber);
+        }
+    }
+}
+console.log(`Vetor de números pares: ${evenNumbers}`);
+console.log(`Vetor de números ímpares: ${oddNumbers}`);
