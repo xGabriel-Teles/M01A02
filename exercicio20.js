903f9d2fd7044f0419c31c75a7e5fb4c80d21ba0
xGabriel-Teles <gabriel.teles.santos@hotmail.com>
2024-06-10 14:01:42 -0300

    exercicio20.js solved

--- exercicio20.js
+++ exercicio20.js
@@ -10,3 +10,21 @@ Salário líquido:
 (Dicas: desconto de 12%, salário líquido é a diferença entre salário bruto e a redução do
 INSS).
 */
+
+const prompt = require('prompt-sync')();
+
+let numOfEmployees = 80;
+let registration = 0;
+let name;
+let grossSalary = 0;
+let netSalary = 0;
+
+for (let i = 1; i <= numOfEmployees; i ++){
+    registration = i;
+    name = prompt('Digite o nome do funcionário: ');
+    grossSalary = parseFloat(prompt('Digite o salário bruto do funcionário: '))
+    netSalary = grossSalary * 0.88;
+
+
+    console.log(`\nMatrícula: ${registration} \nNome: ${name} \nSalário bruto: R$${grossSalary.toFixed(2)} \nDedução INSS: R$${(grossSalary - netSalary).toFixed(2)} \nSalário líquido: R$${netSalary.toFixed(2)}\n`)
+}
