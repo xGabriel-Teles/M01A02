264b7a8e23dc84bd8a6b6bb83ede3eb5930466a3
xGabriel-Teles <gabriel.teles.santos@hotmail.com>
2024-06-11 00:22:24 -0300

    exercicio38.js solved

--- exercicio38.js
+++ exercicio38.js
@@ -7,3 +7,43 @@ variável:
     4- ordene os elementos em ordem crescente;
     5- mostre o vetor.
 */
+const prompt = require('prompt-sync')();
+
+let V = new Array(6);
+
+for (let i = 0; i < 6; i++) {
+    V[i] = parseInt(prompt(`Digite o ${i + 1}º número: `));
+}
+let identifier = prompt('Digite o número identificador entre 1 e 5: ')
+
+let sum = 0;
+
+switch (identifier) {
+    case '1':
+        for (let i = 0; i < V.length; i++) {
+            sum += V[i];
+        }
+        console.log(`\nA soma dos elementos do vetor é: ${sum}`);
+        break
+    case '2':
+        let product = V[0] * V[1] * V[2] * V[3] * V[4] * V[5];
+        console.log(`\nO produto dos elementos do vetor é: ${product}`);
+        break
+    case '3':
+        for (let i = 0; i < V.length; i++) {
+            sum += V[i];
+        }
+        let average = sum / 6;
+        console.log(`\nA média dos elementos do vetor é: ${average}`);
+        break
+    case '4':
+        V.sort(function(a, b) {
+            return a - b;
+          });
+          console.log(`\nO vetor ordenado em ordem crescente é: ${V}`);
+        break
+    case '5':
+        console.log(`\nO vetor é: ${V}`);
+        break
+}
+
