f3365a6bcb5e05de4e7ccdb5b6c74649a778f82e
xGabriel-Teles <gabriel.teles.santos@hotmail.com>
2024-06-11 00:42:19 -0300

    exercicio39.js solved

--- exercicio39.js
+++ exercicio39.js
@@ -2,3 +2,20 @@
 Faça um algoritmo que leia um vetor (A) de 100 posições. 
 Em seguida, compacte o vetor, retirando os valores nulos e negativos. Coloque o resultado no vetor B.
 */
+
+let A = new Array(50);
+let B = new Array();
+
+for (let i = 0; i < A.length; i++) {
+    A[i] = Math.floor(Math.random() * 100 - 50)
+}
+for (let i = 0; i < A.length; i++) {
+    if (A[i] > 0) {
+        B.push(A[i]);
+    }
+}
+
+console.log('\nVetor original: ')
+console.log(A);
+console.log('\nVetor compactado (sem números nulos ou negativos): ')
+console.log(B);
