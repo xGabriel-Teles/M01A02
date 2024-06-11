8b589c6c4e83c2f0ee2925fc2ef3dfd5cf3d86b5
xGabriel-Teles <gabriel.teles.santos@hotmail.com>
2024-06-11 14:05:32 -0300

    exercicio41.js solved

--- exercicio41.js
+++ exercicio41.js
@@ -2,3 +2,18 @@
 Dado o objeto pessoa com propriedades nome e idade, acesse e imprima o valor de idade.
 Adicione uma nova propriedade chamada email ao objeto pessoa que já possui nome e idade.
 */
+
+// Declarando o objeto pessoa;
+let person = {
+    name: 'Jão',
+    age: 22,
+
+}
+// Imprimindo o valor de idade;
+console.log(`Idade: ${person.age}.`)
+
+// Adicionando a propriedade e-mail ao objeto pessoa;
+person.email = 'email@email.com.ui'
+
+// Imprimindo o objeto após o e-mail ser adicionado;
+console.log('Objeto completo após modificação:', person)
