a3865d6dcd7dcfeb678dcc2ccfecc20525dd69e6
xGabriel-Teles <gabriel.teles.santos@hotmail.com>
2024-06-11 20:20:37 -0300

    exercicio42.js solved

--- exercicio42.js
+++ exercicio42.js
@@ -2,3 +2,25 @@
 Crie um objeto chamado dados que contém várias propriedades, incluindo números, strings e arrays.
 Escreva uma função que retorne um novo objeto apenas com as propriedades que são arrays.
 */
+
+let data = {
+    name: 'Jão',
+    age: 30,
+    hobbies: ['futebol', 'leitura', 'games'],
+    address: {
+        street: 'Rua Principal',
+        number: 123
+    },
+    phones: ['99999-9999', '3434-3434'],
+    active: true,
+};
+
+let result = {};
+
+for (let i in data) {
+    if (Array.isArray(data[i])) {
+        result[i] = data[i];
+    }
+}
+
+console.log(result)
