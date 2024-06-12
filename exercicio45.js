dbafb3b8169ecaca012890940407d761ee6863c2
xGabriel-Teles <gabriel.teles.santos@hotmail.com>
2024-06-12 12:39:50 -0300

    exercicio45.js solved

--- exercicio45.js
+++ exercicio45.js
@@ -1,3 +1,17 @@
 /* 45. 
 Dado um array de strings, crie um objeto onde cada string é uma chave, e seu valor é o número de vezes que a string aparece no array.
 */
+
+let A = ['banana', 'pera', 'banana', 'melancia', 'jaca', 'jaca'];
+
+let object = {};
+
+A.forEach(string => {
+    if (object[string]) {
+        object[string]++;
+    } else {
+      object[string] = 1;
+    }
+  });
+
+  console.log(object)
