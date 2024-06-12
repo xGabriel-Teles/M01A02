a9448e2911a4ed216b6a192c02d397c87d165c2d
xGabriel-Teles <gabriel.teles.santos@hotmail.com>
2024-06-11 23:21:16 -0300

    exercicio44.js solved

--- exercicio44.js
+++ exercicio44.js
@@ -1,3 +1,33 @@
 /* 44.
 Escreva uma função que conte quantas propriedades do tipo string existem em um objeto e retorne esse número.
 */
+
+
+function propertyCounter(object) {
+    let counterString = 0;
+    for (let prop in object) {
+        if (object.hasOwnProperty(prop) && typeof object[prop] === 'string') {
+            counterString++;
+        }
+
+    }
+    return counterString;
+}
+
+let object1 = {
+    name: 'Jão',
+    lastName: 'Santos',
+    age: 30,
+    hobbies: ['futebol', 'leitura', 'games'],
+    address: {
+        street: 'Rua dos bobos',
+        number: 0,
+    },
+    phones: ['99999-9999', '3434-3434'],
+    active: true,
+};
+
+let counter = propertyCounter(object1);
+
+console.log(`\nNo objeto declarado, há ${counter} propriedades do tipo string.`);
+console.log();
