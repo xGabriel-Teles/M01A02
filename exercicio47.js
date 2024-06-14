d281407c6fc3b3a61be44562d800b7b3bdcc51fc
xGabriel-Teles <gabriel.teles.santos@hotmail.com>
2024-06-14 12:48:50 -0300

    exercicio47.js solved

--- exercicio47.js
+++ exercicio47.js
@@ -2,3 +2,28 @@
 Crie uma função que transforme um objeto de entrada aplicando uma função fornecida a cada uma das propriedades do objeto, 
 retornando um novo objeto com os resultados.
 */
+
+function transformObject(obj, transformFunc) {
+    
+    const result = {};
+  
+for (const key in obj) {
+    if (obj.hasOwnProperty(key)) {
+        result[key] = transformFunc(obj[key]);
+    }
+};
+    return result;
+};
+
+function transformFunc(x) {
+    return x * 2;
+};
+  
+let originalObject = {
+    a: 2,
+    b: 4,
+    c: 6
+};
+  
+let newObject = transformObject(originalObject, transformFunc);
+console.log(newObject);
